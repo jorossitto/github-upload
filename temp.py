a243ea5493b2571f529d98c8b4ed0d2d8b713cb5
jorossitto <jorossitto@hotmail.com>
2019-06-28 10:09:19 -0400

    New files

---
+++ temp.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+rate = 1.03
+term = 5
+amount = 29.45
+
+interest = rate ** term
+
+print (interest)
+total = amount * interest
+print(total)
