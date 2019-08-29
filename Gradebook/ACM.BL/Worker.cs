4396d19299bc1ef59907eb2111c89de9b6487f46
jorossitto <jorossitto@hotmail.com>
2019-08-29 17:51:40 -0400

    Events delegates and Lambdas in C# part 2

--- Gradebook/ACM.BL/Worker.cs
+++ Gradebook/ACM.BL/Worker.cs
@@ -8,13 +8,15 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    public delegate int WorkPerformedHandler(int hours, WorkType workType);
     public enum WorkType
     {
         GoToMeetings,
         Golf,
         GenerateReports
     }
+
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+
     public class Worker
     {
         public event WorkPerformedHandler WorkPerformed;
@@ -22,7 +24,26 @@ namespace ACM.BL
 
         public void DoWork(int hours, WorkType workType)
         {
+            for(int i = 0; i < hours; i ++)
+            {
+                OnWorkPerformed(i + 1, workType);
+                
+            }
 
+            OnWorkCompleted();
+            //Raise Event
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //WorkPerformed?.Invoke(hours, workType);
+            (WorkPerformed as WorkPerformedHandler)?.Invoke(hours, workType);
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //OnWorkCompleted?.Invoke(this, EventArgs.Empty);
+            (WorkCompleted as EventHandler)?.Invoke(this, EventArgs.Empty);
         }
     }
 }
